88a4afbfa8da258689d971d11aae0668fff04617
Robert <robert.bonham@gmail.com>
2018-02-24 22:52:27 -0800

    Init Typescript for Asp.net Core

---
+++ ProofOfConcepts/AspDotNetCoreMVC/appScriptsJS/app.js
@@ -0,0 +1,8 @@
+function Welcome(person) {
+    return "<h2>Hello " + person + ", this is TypeScript</h2>";
+}
+function ClickMeButton() {
+    var user = "BonhamRL";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
+//# sourceMappingURL=app.js.map
---
+++ ProofOfConcepts/AspDotNetCoreMVC/tsScripts/app.ts
@@ -0,0 +1,8 @@
+﻿function Welcome(person: string) {
+    return "<h2>Hello " + person + ", this is TypeScript</h2>";
+}
+
+function ClickMeButton() {
+    let user: string = "BonhamRL";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
